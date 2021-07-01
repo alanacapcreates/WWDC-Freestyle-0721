ab4a8eb50987872fd58a3de96deaf8c3c989564b
Alana Caporale <alanacaporale@gmail.com>
2021-07-01 17:00:05 -0400

    stylized the choose story section

--- src/ChooseStory.js
+++ src/ChooseStory.js
@@ -1,7 +1,13 @@
 import React from "react"
 
-function handleClick() {
-    alert("clicked the button")
+function story1() {
+    alert("clicked story 1")
+}
+function story2() {
+    alert("clicked story 2")
+}
+function story3() {
+    alert("clicked story 3")
 }
 
 function ChooseStory() {
@@ -10,15 +16,18 @@ function ChooseStory() {
             <h2>Choose a Mad Lib story below</h2>
             <div>
                 <h3>Story 1</h3>
-                <button onClick={handleClick}>Create Lib</button>
+                <p class = "story-desc">The description for this mad lib story will go here</p>
+                <button onClick={story1}>Create Lib</button>
             </div>
             <div>
                 <h3>Story 2</h3>
-                <button>Create Lib</button>
+                <p class = "story-desc">The description for this mad lib story will go here</p>
+                <button onClick={story2}>Create Lib</button>
             </div>
             <div>
                 <h3>Story 3</h3>
-                <button>Create Lib</button>
+                <p class = "story-desc">The description for this mad lib story will go here</p>
+                <button onClick={story3}>Create Lib</button>
             </div>
         </div>
     )
