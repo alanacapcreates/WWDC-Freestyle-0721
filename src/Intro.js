8bcada34fefd44b0613735f86ba8a87d09f99192
Alana Caporale <alanacaporale@gmail.com>
2021-07-03 19:08:31 -0400

    updated styling: border-left gradient

--- src/Intro.js
+++ src/Intro.js
@@ -2,11 +2,10 @@ import React from "react"
 
 function Intro() {
     return (
-        <div class="container">
-            <h1><span class="gradient">The Weekly</span> <span class="outline">Web Dev Challenge</span></h1>
-            <p>Freestyle Challenge</p>
-            <p><em>Last Updated: July, 03, 2021</em></p>
-            <hr/>
+        <div class="container intro">
+            <h1 id="intro-header"><span class="gradient">The Weekly</span> <span class="outline">Web Dev Challenge</span></h1>
+            <p id="intro-challenge">Freestyle Challenge</p>
+            <p id="intro-updated">Last Updated: July, 03, 2021</p>
         </div>
     )
 }
