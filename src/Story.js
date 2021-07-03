05e0b1e668f2fae9f2854ab65d3cf8973d4cc7aa
Alana Caporale <alanacaporale@gmail.com>
2021-07-03 12:23:09 -0400

    modified onclick to dynamically handle story ID

--- src/Story.js
+++ src/Story.js
@@ -1,15 +1,16 @@
 import React from "react"
 
 function handleClick(){
-    alert("clicked")
+    
 }
 
-function Story(props) {
+function Story({story, id, onClick}) {
+
     return ( 
             <div>
-                <h3>{props.story.title}</h3>
-                <p class = "story-desc">{props.story.description}</p>
-                <button onClick={handleClick}>Create Lib</button>
+                <h3>{story.title}</h3>
+                <p class = "story-desc">{story.description}</p>
+                <button onClick={() => onClick(story.id)}>Create Lib</button>
             </div>
         
     )
