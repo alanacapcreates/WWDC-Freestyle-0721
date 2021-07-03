721233b15eba0b0bbdd3bcf6006fb60e72c5fa28
Alana Caporale <alanacaporale@gmail.com>
2021-07-03 12:21:03 -0400

    mapping out the object arrays for each story

--- src/storyData.js
+++ src/storyData.js
@@ -1,22 +1,33 @@
-const stories = [
+export const storyData = [
     {
-        id: "1",
+        id: 1,
         title: "Pumpkin's Day Off",
         description: "Pumpkin has a long holiday weekend coming up. Help decide what he should do on his days off!",
         length: 10
     },
     {
-        id: "2",
+        id: 2,
         title: "Leanne's Day Off",
         description: "Leanne has a long holiday weekend coming up. Help decide what she should do on her days off!",
         length: 15
     },
     {
-        id: "3",
+        id: 3,
         title: "Michael's Day Off",
         description: "Michael has a long holiday weekend coming up. Help decide what he should do on his days off!",
         length: 15
     }
-]
+];
 
-export default stories
+export const storyConfig = {
+    1: {
+        storyText:"Today my $. The ^ is nice."
+    },
+    2:{
+        storyText:"This is story 2"
+    },
+    3:{
+        storyText:"This is story 3"
+    }
+    
+}
