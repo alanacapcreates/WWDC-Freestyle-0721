6022be36da0e3d69319d05f2aa6340df4a33db31
Alana Caporale <alanacaporale@gmail.com>
2021-07-03 23:47:20 -0400

    Checking if user filled out all inputs

--- src/Form.js
+++ src/Form.js
@@ -3,8 +3,8 @@ import CompletedStory from "./CompletedStory"
 import { storyConfig } from "./storyData"
 
 const defaultLibMap = {
+    a1: "",
     v1: "",
-    c1: "",
     p1: "",
     n1: ""
 }
@@ -14,6 +14,11 @@ const defaultLibMap = {
  * @returns 
  */
 function Form({setActiveView,id}) {
+    let a1 = document.getElementById("a1")
+    let v1 = document.getElementById("v1")
+    let p1 = document.getElementById("p1")
+    let n1 = document.getElementById("n1")
+
 
     const [libMap, setLibMap] = useState(defaultLibMap)
     console.log(libMap)
@@ -27,16 +32,20 @@ function Form({setActiveView,id}) {
         //console.log("ID: " + event.target.id + " Value: " + event.target.value)
     }
     const generateStory = () => {
-        setActiveView(<CompletedStory id = {id}/>)
+        if(a1.value == "" || v1.value == "" || p1.value == "" ||n1.value == ""){
+            alert("Please fill out al input fields before continuing")
+        }else{
+            setActiveView(<CompletedStory id = {id} libMap = {libMap}/>)
+        }
     }
 
     return ( 
             <div class="container form-container">
             
+                <input id="a1"type="text" placeholder="adjective" onChange={handleInput}></input>
                 <input id="v1"type="text" placeholder="verb" onChange={handleInput}></input>
-                <input id="c1"type="text" placeholder="color" onChange={handleInput}></input>
                 <input id="p1"type="text" placeholder="place" onChange={handleInput}></input>
-                <input id="n1"type="text" placeholder="name" onChange={handleInput}></input>
+                <input id="n1"type="text" placeholder="number" onChange={handleInput}></input>
 
                 <button onClick={generateStory}>Generate</button>
             </div>
