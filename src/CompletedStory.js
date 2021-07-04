8a1d51a37efe5ba739098fb751f05673d25434dd
Alana Caporale <alanacaporale@gmail.com>
2021-07-03 23:49:16 -0400

    story => array, replacing with user input

--- src/CompletedStory.js
+++ src/CompletedStory.js
@@ -2,12 +2,51 @@ import React,{useState} from "react"
 import { storyConfig } from "./storyData";
 import './App.css';
 
-function CompletedStory({id}) {
+function refresh(){
+  window.location.reload()
+}
 
-  return (
-    <div>
-      <h3>{storyConfig[id].storyText}</h3>
+function CompletedStory({id, libMap}) {
+
+const story = storyConfig[id].storyText
+const arrStory = story.split(" ")
+console.log(arrStory)
+
+/**
+ *  $A = adjective
+ *  $V = verb
+ *  $P = place
+ *  $N = number
+ */
+console.log(libMap)
+for(let i=0;i<arrStory.length;i++){
+  if(arrStory[i] ==="$A"){
+    arrStory[i] = libMap["a1"]
+  }
+  else if(arrStory[i] ==="$V"){
+    arrStory[i] = libMap["v1"]
+  }
+  else if(arrStory[i] ==="$P"){
+    arrStory[i] = libMap["p1"]
+  }
+  else if(arrStory[i] ==="$N"){
+    arrStory[i] = libMap["n1"]
+  }
+}
+let updatedStory = ""
+  for(let i=0;i<arrStory.length;i++){
+    updatedStory+=arrStory[i] + " "
+  }
+
+console.log(updatedStory)
+
+return (
+  <div>
+    <div class="story-container">
+      <p class="updatedStory">{updatedStory}</p>
     </div>
+  <button onClick={refresh}>Start Over</button>
+  </div>
   );
 }
 
