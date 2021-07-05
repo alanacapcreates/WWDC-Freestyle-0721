ee07475e670731691d40c14e7ea94ffacc62f866
Alana Caporale <alanacaporale@gmail.com>
2021-07-05 03:13:09 -0400

    made changes to the stories

--- src/storyData.js
+++ src/storyData.js
@@ -2,32 +2,29 @@ export const storyData = [
     {
         id: 1,
         title: "Pumpkin's Day Off",
-        description: "Pumpkin has a long holiday weekend coming up. Help decide what he should do on his days off!",
-        length: 10
+        description: "Pumpkin has a day off from coding Scrimba Challenges. Help decide what he should do on his day off!",
     },
     {
         id: 2,
-        title: "Leanne's Day Off",
-        description: "Leanne has a long holiday weekend coming up. Help decide what she should do on her days off!",
-        length: 15
+        title: "The 4th of July Mystery",
+        description: "While leaving for a 4th of July party, Pumpkin and some friends notice something strange... what could it be?",
     },
     {
         id: 3,
-        title: "Michael's Day Off",
-        description: "Michael has a long holiday weekend coming up. Help decide what he should do on his days off!",
-        length: 15
+        title: "A Summer Trip",
+        description: "What will Pumpkin do when two of his friends leave for summer vacation? Lets find out!",
     }
 ];
 
 export const storyConfig = {
     1: {
-        storyText:"It was a $A in London. Leanne and Michael are working on Scrimba challenges, but I have the day off! I think I might try to $V to the $P so I can have a yummy lunch. Once I get lunch, I still have $N hours until I need to go to my next Scrimba lesson."
+        storyText:"It was a $A day in London. Leanne and Michael are working on Scrimba challenges, but I have the day off! I think I might try to go to the $P so I can have a yummy lunch. Once I get lunch, I still have $N hours until I need to go to my next Scrimba lesson. While I was walking to $P , I noticed there was a(n) $AM wearing a $C Scrimba hat! How cool is that! "
     },
     2:{
-        storyText:"This is story 2"
+        storyText:"I was going to $P with Michael, Leanne, and three $A dogs. The $A dogs found some $AM in the distance. They ran so $ADV to try and catch up to it. When they finally reached the $C animal they were surprised to see that it was a(n) $O instead. They were so relieved to see this and walked away. Now that we solved that mystery, we finally went to the $P to watch fireworks and eat cake!"
     },
     3:{
-        storyText:"This is story 3"
+        storyText:"One day, $N and $N2 decided to go on their summer vacation to $P . $N grabbed a $DR and hit the road! Since $N and $N2 are leaving for $N days, I decided I was going to take a trip to space! Before I leave, I must pack the essentials: my $O ! I heard planet $P2 can be $A , so I made sure to pack a(n) $O2 . I'll be sure to take a bunch of pics on my $D to post on Insta!"
     }
     
 }
